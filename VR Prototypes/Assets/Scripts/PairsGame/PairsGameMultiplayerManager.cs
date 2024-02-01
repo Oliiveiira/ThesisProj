a647bc19ad006f145859cd27df773d2088137917
Oliiveiira <sergiooliveira4820@gmail.com>
2024-02-01 18:19:09 +0000

    PairsGame basis completed. Network part is needed.

--- VR Prototypes/Assets/Scripts/PairsGame/PairsGameMultiplayerManager.cs
+++ VR Prototypes/Assets/Scripts/PairsGame/PairsGameMultiplayerManager.cs
@@ -1,16 +1,19 @@
+using Oculus.Interaction.HandGrab;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using Random = System.Random;
+using RoboRyanTron.Unite2017.Events;
 
 public class PairsGameMultiplayerManager : PairsListReader
 {
     public List<GameObject> pairsObjects;
     public List<GameObject> jsonPrefabs = new List<GameObject>();
     [SerializeField]
-    private List<Transform> leftPlaceholders;
+    private Transform[] leftPlaceholders;
     [SerializeField]
-    private List<Transform> rightPlaceholders;
+    private Transform[] rightPlaceholders;
     [SerializeField]
     private GameObject piecesParent;
     public Vector3 scale = new Vector3(10f, 10f, 10f); // Example scale values
@@ -28,6 +31,16 @@ public class PairsGameMultiplayerManager : PairsListReader
     private List<GameObject> rightPieces;
     public bool isInLeftPlace;
     public bool isInRightPlace;
+    private AudioSource popSound;
+    [SerializeField]
+    private Renderer leftPieceMaterial;
+    [SerializeField]
+    private Renderer rightPieceMaterial;
+    private static Random random = new Random();
+    [SerializeField]
+    private GameEvent setWinPanel;
+    [SerializeField]
+    private List<int> usedPieceIndexes;
 
     private void Awake()
     {
@@ -54,17 +67,31 @@ public class PairsGameMultiplayerManager : PairsListReader
         string jsonText = File.ReadAllText(jsonFilePath);
         myPairsList = JsonUtility.FromJson<PairsList>(jsonText);
 
+        popSound = GetComponent<AudioSource>();
+
         LoadPrefabsFromJSON();
+        AssignPlaceholders();
+        ChoosePair();
         //AssignPlaceholders();
     }
 
     private void LoadPrefabsFromJSON()
     {
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < leftPlaceholders.Length; i++)
         {
-            GameObject pairsToInstantiate = Resources.Load<GameObject>(myPairsList.pairlevel[i].pairPath);
+            // Get the count of pair paths for the current index
+            int pairPathCount = myPairsList.pairlevel.Count;
+            int randomPairIndex = random.Next(0, pairPathCount);
+
+            do
+            {
+                randomPairIndex = random.Next(0, pairPathCount);
+            } while (usedIndexes.Contains(randomPairIndex)); // Check if the index has been used before
+
+            GameObject pairsToInstantiate = Resources.Load<GameObject>(myPairsList.pairlevel[randomPairIndex].pairPath);
             //guardar indexes que serão utilizados para dar load dos vários pares, para posteriormente escolher cada um recorrentemente
-            usedIndexes.Add(i);
+           
+            usedIndexes.Add(randomPairIndex);
             jsonPrefabs.Add(pairsToInstantiate);
         }
     }
@@ -75,66 +102,133 @@ public class PairsGameMultiplayerManager : PairsListReader
         // Choose a random index from the remaining prefabs list
         if(jsonPrefabs.Count > 0 && !pickPair)
         {
-            randomIndex = Random.Range(0, jsonPrefabs.Count);
-
-            gridPositions[0].name = leftPieces[randomIndex].transform.name;
-            gridPositions[1].name = rightPieces[randomIndex].transform.name;
+            Debug.Log(jsonPrefabs.Count - 1);
+            Debug.Log("choosePair");
+            randomIndex = random.Next(0, jsonPrefabs.Count);
+            Debug.Log(randomIndex);
+            gridPositions[0].name = leftPieces[jsonPrefabs.Count - 1].transform.name;
+            gridPositions[1].name = rightPieces[jsonPrefabs.Count - 1].transform.name;
+            SetPairToMakeMaterial();
 
             //jsonPrefabs.RemoveAt(randomIndex);
             pickPair = true;
         }
+        else
+        {
+            Debug.Log("Winner!");
+            setWinPanel.Raise();
+        }
+    }
+
+    public void SetPairToMakeMaterial()
+    {
+        Renderer leftRendererMaterials = leftPieces[jsonPrefabs.Count - 1].GetComponent<Renderer>();
+        leftPieceMaterial.materials = leftRendererMaterials.materials;
+        Renderer rightRendererMaterials = rightPieces[jsonPrefabs.Count - 1].GetComponent<Renderer>();
+        rightPieceMaterial.materials = rightRendererMaterials.materials;
     }
 
     private void ComparePositions()
     {
-        if(Vector3.Distance(leftPieces[randomIndex].transform.position, gridPositions[0].transform.position) < 0.02f)
+        if(Vector3.Distance(leftPieces[jsonPrefabs.Count - 1].transform.position, gridPositions[0].transform.position) < 0.02f && gridPositions[0].name == leftPieces[jsonPrefabs.Count - 1].transform.name && !isInLeftPlace)
         {
-            leftPieces[randomIndex].transform.position = gridPositions[0].transform.position;
+            Debug.Log("leftPiece");
+            leftPieces[jsonPrefabs.Count - 1].transform.position = gridPositions[0].transform.position;
+            popSound.Play();
+            HandGrabInteractable handGrabInteractable = leftPieces[jsonPrefabs.Count - 1].GetComponent<HandGrabInteractable>();
+            if (handGrabInteractable != null)
+            {
+                handGrabInteractable.enabled = false;
+            }
+
             isInLeftPlace = true;
         }
-        else if(Vector3.Distance(rightPieces[randomIndex].transform.position, gridPositions[1].transform.position) < 0.02f)
+        if(Vector3.Distance(rightPieces[jsonPrefabs.Count - 1].transform.position, gridPositions[1].transform.position) < 0.02f && gridPositions[1].name == rightPieces[jsonPrefabs.Count - 1].transform.name && !isInRightPlace)
         {
-            rightPieces[randomIndex].transform.position = gridPositions[1].transform.position;
+            Debug.Log("rightPiece");
+            rightPieces[jsonPrefabs.Count - 1].transform.position = gridPositions[1].transform.position;
+            popSound.Play();
+            HandGrabInteractable handGrabInteractable = rightPieces[jsonPrefabs.Count - 1].GetComponent<HandGrabInteractable>();
+            if (handGrabInteractable != null)
+            {
+                handGrabInteractable.enabled = false;
+            }
+
             isInRightPlace = true;
         }
-        else if(isInLeftPlace && isInRightPlace)
+        if(isInLeftPlace && isInRightPlace && pickPair)
         {
+            isInLeftPlace = false;
+            isInRightPlace = false;
+            int lastRandomIndex = randomIndex;
+            Debug.Log("Entrou aqui");
+            //leftPieces[randomIndex].SetActive(false);
+            //rightPieces[randomIndex].SetActive(false);
+            //randomIndex = random.Next(0, jsonPrefabs.Count);
+            StartCoroutine(DeactivatePair(jsonPrefabs.Count - 1));
+            jsonPrefabs.RemoveAt(jsonPrefabs.Count - 1);
             pickPair = false;
+            ChoosePair();
         }
-
     }
 
     private void AssignPlaceholders()
     {
+        ShuffleArray(leftPlaceholders);
+        ShuffleArray(rightPlaceholders);
+
+        int i = 0;
         foreach(GameObject piece in jsonPrefabs)
         {
-            GameObject leftPiece = Instantiate(piece.transform.GetChild(0).gameObject, leftPlaceholders[0].position, Quaternion.Euler(0, -90, 90));
+            GameObject leftPiece = Instantiate(piece.transform.GetChild(0).gameObject, leftPlaceholders[i].position, Quaternion.Euler(0, -90, 90));
             leftPiece.transform.parent = piecesParent.transform;
             leftPiece.transform.localScale = scale;
             leftPiece.name = piece.transform.GetChild(0).name;
             leftPieces.Add(leftPiece);
 
-            GameObject rightPiece = Instantiate(piece.transform.GetChild(1).gameObject, rightPlaceholders[0].position, Quaternion.Euler(0, -90, 90));
+            GameObject rightPiece = Instantiate(piece.transform.GetChild(1).gameObject, rightPlaceholders[i].position, Quaternion.Euler(0, -90, 90));
             rightPiece.transform.parent = piecesParent.transform;
             rightPiece.transform.localScale = scale;
             rightPiece.name = piece.transform.GetChild(1).name;
             rightPieces.Add(rightPiece);
+            i++;
         }
         //GameObject piece1 = Instantiate(jsonPrefabs[0].transform.GetChild(0).gameObject, leftPlaceholders[0].position, Quaternion.Euler(0,-90,90));
         //piece1.transform.parent = piecesParent.transform;
         //piece1.transform.localScale = scale;
     }
 
+    IEnumerator DeactivatePair(int lastRandomIndex)
+    {
+        yield return new WaitForSeconds(2);
+        leftPieces[lastRandomIndex].SetActive(false);
+        rightPieces[lastRandomIndex].SetActive(false);
+        //yield return new WaitForSeconds(2);
+        //ChoosePair();
+        //pickPair = false;
+        //jsonPrefabs.RemoveAt(randomIndex);
+        //ChoosePair();
+    }
+
+    public static void ShuffleArray<T>(T[] array)
+    {
+        Random random = new Random();
+
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = random.Next(0, i + 1);
+
+            // Swap array[i] and array[j]
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            AssignPlaceholders();
-            ChoosePair();
-            ComparePositions();
-        }
+        ComparePositions();
     }
 }
